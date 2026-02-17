041e1349142e8d11c690f41585e5a3d62157a9eb
aquamancer <aquamancer90@gmail.com>
2026-02-17 04:03:35 -0500

    remove respawningpoi.java

--- src/main/java/com/aquamancer/antilootruntracker/poirespawn/PoiRespawnTracker.java
+++ src/main/java/com/aquamancer/antilootruntracker/poirespawn/PoiRespawnTracker.java
@@ -1,13 +1,6 @@
 package com.aquamancer.antilootruntracker.poirespawn;
 
-import com.aquamancer.antilootruntracker.ShardInfo;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class PoiRespawnTracker {
